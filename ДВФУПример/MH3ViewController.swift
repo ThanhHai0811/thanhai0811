d30590052f414af3f40b6e76c03d4a29bc8ee77f
ThanhHai0811 <92018302+ThanhHai0811@users.noreply.github.com>
2021-10-09 00:22:14 +1000

    Homework0210

---
+++ ДВФУПример/MH3ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MH3ViewController.swift
+//  ДВФУПример
+//
+//  Created by Фам Тхань Хай on 07/10/2021.
+//
+
+import UIKit
+
+class MH3ViewController: UIViewController {
+
+    @IBOutlet weak var MyImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        MyImage.image = UIImage(named: "people")
+    }
+    
+
+   
+}
